5fe99b1f2cf85618c73de9a4cfbaf30675f3df53
maggie <ranirathor20@navgurukul.org>
2023-10-31 15:07:32 +0530

    UI completed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,31 @@
+"use client"
+import React from 'react';
+import Dropdowns from './dropD.js';
+
 const App=()=>{
     return (
     <>
         <div className="App">
             <div className="container">
                 <h1>Todo List</h1>
+                <form className="todoForm">
+                    <input type="text" />
+                    <button>Add</button>
+                </form>
+                <Dropdowns />
+                <ul className="allTodos">
+                    <li className="singleTodo">
+                        <span className="todoText">Status</span>
+                        <button>Delete</button>
+                    </li>
+                    <li className="singleTodo">
+                        <span className="todoText">Status</span>
+                        <button>Delete</button>
+                    </li>
+                </ul>
             </div>
         </div>
     </>
     )
 }
-
 export default App;
