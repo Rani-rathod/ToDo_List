8d163f3463415e79f19157298551b1506a4bf0da
maggie <ranirathor20@navgurukul.org>
2023-11-01 11:11:19 +0530

    Adding element is done

--- app/page.js
+++ app/page.js
@@ -1,27 +1,36 @@
 "use client"
-import React from 'react';
+import React, {useState} from 'react';
 import Dropdowns from './dropD.js';
 
 const App=()=>{
+    const [todo,setTodo]=useState("")
+    const [todos,setTodos]=useState([])
+    
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        
+        if (todo!==""){
+            setTodos([{id:`${todo}-${Date.now()}`,todo}, ...todos])
+        }
+    }
     return (
     <>
         <div className="App">
             <div className="container">
+               
                 <h1>Todo List</h1>
-                <form className="todoForm">
-                    <input type="text" />
-                    <button>Add</button>
+                <form className="todoForm" onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Enter a Todo..." className="text-input" onChange={(e)=>setTodo(e.target.value)}/>
+                    <button type="submit">Add</button>
                 </form>
                 <Dropdowns />
                 <ul className="allTodos">
+                    {todos.map((t)=>(
                     <li className="singleTodo">
-                        <span className="todoText">Status</span>
+                        <span className="todoText">{t.todo}</span>
                         <button>Delete</button>
                     </li>
-                    <li className="singleTodo">
-                        <span className="todoText">Status</span>
-                        <button>Delete</button>
-                    </li>
+                    ))}
                 </ul>
             </div>
         </div>
@@ -29,3 +38,4 @@ const App=()=>{
     )
 }
 export default App;
+
